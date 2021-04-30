10e0cd6cde4939cae5be16bf1ecd401e46eec32e
Eline302 <1659979517@qq.com>
2021-04-30 14:18:51 +0800

    blog后端服务代码提交

---
+++ config/database.js
@@ -0,0 +1,36 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+      default: {
+          connector: 'bookshelf',
+          settings: {
+              client: 'mysql',
+              host: env('DATABASE_HOST', 'localhost'),
+              port: env.int('DATABASE_PORT', 3306),
+              // 你的数据库名称
+              database: env('DATABASE_NAME', 'blog'),
+              // 你的服务器中的数据库的用户名和密码
+              username: env('DATABASE_USERNAME', 'blog'),
+              password: env('DATABASE_PASSWORD', '123456blog'),
+          },
+          options: {},
+      },
+  },
+});
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ec361932-1fa4-4685-8c06-8ca118cdde8f'
+};
